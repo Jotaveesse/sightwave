3db56e4a0033026973b05a14efae39725260e172
Jotaveesse <jvs2@cin.ufpe.br>
2023-08-29 09:59:23 -0300

    Melhorias no css e funcionalidades da página

--- frontend/image_to_lyrics.js
+++ frontend/image_to_lyrics.js
@@ -1,55 +1,183 @@
 var inputField;
 var textArea;
 var embedTemplate;
-const apiUrl = window.location.href+'api/search/';
+var imageUpload;
+var imageUploadTop;
+var imageInput;
+var imageDisplayArea;
+var imageDisplay;
+var sendAnotherButton;
+var imageSendButton;
+var urlInput;
+var fileDropArea;
+var sendButton;
+var lyricsText;
+var embedSection;
+var buttonArea;
+var loadingArea;
+
+var currUrl = null;
+var currFile = null;
+
+//const apiUrl = window.location.href + 'api/search/';
+const apiUrl = 'https://image-to-lyrics.vercel.app/api/search/'
 
 window.onload = function () {
     inputField = document.getElementById('input-field');
     textArea = document.getElementById('text-area');
     embedTemplate = document.getElementById('embed-iframe-template');
+    imageUpload = document.getElementById('image-upload');
+    imageUploadTop = document.getElementById('image-upload-top');
+    imageInput = document.getElementById('image-input');
+    imageDisplayArea = document.getElementById('image-display-area');
+    imageDisplay = document.getElementById('image-display');
+    sendAnotherButton = document.getElementById('send-another-button');
+    fileDropArea = document.getElementById('file_drop_area');
+    imageSendButton = document.getElementById('url-send-button');
+    sendButton = document.getElementById('end-button');
+    urlInput = document.getElementById('url-input').children[0];
+    lyricsText = document.getElementById('lyrics-text');
+    embedSection = document.getElementById('embed-section');
+    buttonArea = document.getElementById('button-area');
+    loadingArea = document.getElementById('loading-area');
+
+    imageUpload.addEventListener('dragover', function (e) {
+        e.preventDefault();
+    });
+
+    imageUploadTop.addEventListener('dragenter', function (e) {
+        e.preventDefault();
+        var children = imageUploadTop.children;
+        for (const child of children) {
+            child.classList.add('brightened');
+        }
+
+    });
+
+    imageUploadTop.addEventListener('dragleave', function (e) {
+        e.preventDefault();
+        var children = imageUploadTop.children;
+        for (const child of children) {
+            child.classList.remove('brightened');
+        }
+    });
+
+    imageUploadTop.addEventListener('drop', function (e) {
+        e.preventDefault();
+
+        const files = e.dataTransfer.files;
+        handleFiles(files);
+    });
+
+    sendAnotherButton.addEventListener('click', function (e) {
+        currUrl = null;
+        toggleVisibility(imageDisplay);
+        toggleVisibility(imageUpload);
+    });
+
+    imageSendButton.addEventListener('click', function (e) {
+        isImageURLValid(urlInput.value, function (isValid) {
+            if (isValid) {
+                currUrl = urlInput.value;
+                imageDisplayArea.src = currUrl;
+                toggleVisibility(imageDisplay);
+                toggleVisibility(imageUpload);
+            } else {
+                alert('URL inválida');
+            }
+        });
+    });
+
+
+    // Function to handle dropped files
+    function handleFiles(files) {
+        if (files && files[0]) {
+            const selectedFile = files[0];
+
+            const reader = new FileReader();
+
+            reader.onload = function (event) {
+                imageDisplayArea.src = event.target.result;
+                toggleVisibility(imageDisplay);
+                toggleVisibility(imageUpload);
+            }
+
+            // Read the selected file as a data URL
+            reader.readAsDataURL(selectedFile);
+        }
+    }
 };
+
+function isImageURLValid(url, callback) {
+    var img = new Image();
+    img.onload = function () {
+        callback(true);
+    };
+    img.onerror = function () {
+        callback(false);
+    };
+    img.src = url;
+}
+
+function toggleVisibility(element) {
+    element.classList.toggle('hidden');
+}
 
 function createEmbed(trackId) {
     var embed = embedTemplate.content.cloneNode(true);
     var clonedIframe = embed.querySelector('iframe');
-    clonedIframe.src =  clonedIframe.src.replace('TRACKID', trackId)
-    
-    document.body.appendChild(clonedIframe);
+    clonedIframe.src = clonedIframe.src.replace('TRACKID', trackId)
+
+    embedSection.appendChild(clonedIframe);
 }
 
 function requestLyricsFromImage() {
+    if (currUrl == null)
+        return;
+
     queryParams = {
         url: inputField.value
     }
-    getRequest(apiUrl+'image', queryParams).then(data => {
+    /*getRequest(apiUrl+'image', queryParams).then(data => {
         displayTracks([data]);
 
     }).catch(error => {
         textArea.value = 'An error occurred while fetching lyrics: ' + error;
-    });
+    });*/
 }
 
 function requestLyricsFromLyrics() {
+    //if (currUrl == null)
+    //    return;
+
     queryParams = {
-        query:  inputField.value
+        query: urlInput.value
     }
-    getRequest(apiUrl+'lyrics', queryParams).then(data => {
+
+    toggleVisibility(loadingArea)
+    toggleVisibility(buttonArea)
+    getRequest(apiUrl + 'lyrics', queryParams).then(data => {
+        toggleVisibility(loadingArea)
+        toggleVisibility(buttonArea)
         displayTracks([data]);
-        
+
     }).catch(error => {
-        textArea.value = 'An error occurred while fetching lyrics: ' + error;
+        lyricsText.innerHTML = 'An error occurred while fetching lyrics: ' + error;
     });
 }
 
-function requestLyricsFromFeats(){
+function requestLyricsFromFeats() {
+    if (currUrl == null)
+        return;
+
     queryParams = {
-        query:  inputField.value
+        query: inputField.value
     }
-    getRequest(apiUrl+'features', queryParams).then(data => {
+    getRequest(apiUrl + 'features', queryParams).then(data => {
         displayTracks([data]);
-        
+
     }).catch(error => {
-        textArea.value = 'An error occurred while fetching lyrics: ' + error;
+        lyricsText.innerHTML = 'An error occurred while fetching lyrics: ' + error;
     });
 }
 
@@ -58,16 +186,17 @@ function displayTracks(tracks) {
     iframes.forEach(element => {
         element.remove();
     });
-    
-    textArea.value = '';
-    
+
+    lyricsText.value = '';
+
     console.log(tracks);
 
     tracks.forEach(element => {
 
         if (element != null) {
             createEmbed(element.id);
-            textArea.value += element.matched_section + '\n';
+            console.log(element);
+            lyricsText.innerHTML += element.lyrics.join("<br>");
         }
     });
 }
